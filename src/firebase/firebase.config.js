92fdafb6be407182812e1364db26fce3c60fbab7
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-12-31 13:43:21 +0600

    fix issue in myBooking and firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,12 +8,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBAvrw96YW86615DZWwV-LmvkY7k0Kpl1c",
-    authDomain: "book-your-hotel-18c2b.firebaseapp.com",
-    projectId: "book-your-hotel-18c2b",
-    storageBucket: "book-your-hotel-18c2b.firebasestorage.app",
-    messagingSenderId: "116906370082",
-    appId: "1:116906370082:web:4f7b65947d52b32151d67b"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
